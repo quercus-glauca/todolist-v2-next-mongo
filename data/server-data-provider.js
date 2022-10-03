d0d6b7270107a01c68491a71bec3c06d9b2f6793
Quercus Glauca <quercusglauca1153@gmail.com>
2022-10-03 17:43:04 +0200

    deleteTodoListItem at Server returns an Object, not a simple itemId Number

--- data/server-data-provider.js
+++ data/server-data-provider.js
@@ -1,41 +1,41 @@
-const sampleSimpleListItem  = {
+const sampleSimpleListItem = {
   date: new Date(),
   text: "Sample List Item",
 };
 
-const sampleListItem  = {
+const sampleListItem = {
   _id: 0,
   date: new Date(),
   text: "Sample List Item",
 };
 
 const defaultSimpleTodoListItems = [
-  { 
+  {
     date: new Date(),
     text: "Welcome to your ToDo list!",
   },
-  { 
+  {
     date: new Date(),
     text: "Click the '+' button to add a new item.",
   },
-  { 
+  {
     date: new Date(),
     text: "← Check here to delete the item.",
   },
 ];
 
 let defaultTodoListItems = [
-  { 
+  {
     _id: 0,
     date: new Date(),
     text: "Welcome to your ToDo list!",
   },
-  { 
+  {
     _id: 1,
     date: new Date(),
     text: "Click the '+' button to add a new item.",
   },
-  { 
+  {
     _id: 2,
     date: new Date(),
     text: "← Check here to delete the item.",
@@ -52,7 +52,7 @@ export async function getTodoListItems(listId) {
   // Find all the Documents in the correct Collection
   // Return the results
   // <<TODO>>
-  
+
   // Dummy fallback implementation
   return defaultTodoListItems;
 }
@@ -61,7 +61,7 @@ export async function postTodoListItem(simpleListItem, listId) {
   // Connect to the MongoDB Database
   // Insert the Document 'simpleListItem' in the correct Collection
   // <<TODO>>
-  
+
   // Dummy fallback implementation
   const listItem = {
     _id: defaultTodoListItems.length,
@@ -78,5 +78,5 @@ export async function deleteTodoListItem(itemId, listId) {
 
   // Dummy fallback implementation
   defaultTodoListItems = defaultTodoListItems.filter((item) => (item._id !== itemId));
-  return itemId;    
+  return { itemId };
 }
